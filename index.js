f44a50427a4a684cc03e9ad3f7d3f3ec2afcb204
Gabriel Noss <98369941+GabrielNoss@users.noreply.github.com>
2023-10-27 21:09:12 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const btn = document.getElementById('button');
+const inputEl = document.getElementById('email');
+const message = document.getElementById('message')
+
+btn.addEventListener("click", function(){
+  if(inputEl.value === ""){
+    message.innerHTML = "Whoops! It looks like you forgot to add your email"
+  } else {
+    ValidateEmail();
+  }
+  inputEl.value = "";
+  console.log (message)
+});
+
+function ValidateEmail() {
+  let valid = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if(inputEl.value.match(valid)){
+    message.innerHTML = "Stay alert for our launch date";
+  } else {
+    message.innerHTML = "Please provide a valid email address";
+  }
+}
